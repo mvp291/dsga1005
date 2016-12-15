bebf22d3eeb0c413b3a6707361c287fa40803488
sb5518 <sb5518@nyu.edu>
2016-12-15 12:12:00 -0500

    Adding improved version of Additive noise. Supports dataframe and independence test between X and Y

--- code/additive_noise.py
+++ code/additive_noise.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import pyGPs
 import sklearn
 import hsic as hsic2
@@ -18,35 +19,47 @@ def compute_gp_regression(X_train, y_train, X_test):
     return y_pred
 
 
-def ANM_algorithm(X, y):
+def ANM_algorithm(X, y, hsic='py'):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    leakage_prob = dict()
-    for col in range(X_train.shape[1]):
-        x_train_column = X_train[:, col]
-        x_test_column = X_test[:, col]
+    leakage_dict = dict()
+    variables = X.columns
+    for col in variables:
+        x_train_column = np.array(X_train[col])
+        x_test_column = np.array(X_test[col])
 
         y_pred = compute_gp_regression(x_train_column, y_train, x_test_column)
         x_pred = compute_gp_regression(y_train, x_train_column, y_test)
 
         y_residuals = y_test - y_pred.ravel()  # esto no deberia ser absolute value?
         x_residuals = x_test_column - x_pred.ravel()
+        
+        if hsic == 'py':
+            HSIC_x_to_y = HSIC_b(x_test_column, y_residuals)
+            HSIC_y_to_x = HSIC_b(y_test, x_residuals)
+            if HSIC_x_to_y.empirical_test() < HSIC_y_to_x.empirical_test():
+                leakage_dir = 'No Leakage'
+            else:
+                leakage_dir = 'Leakage'
+            rank = HSIC_x_to_y.empirical_test() - HSIC_y_to_x.empirical_test()
 
-        HSIC_x_to_y = HSIC_b(x_test_column, y_residuals)
-        HSIC_y_to_x = HSIC_b(y_test, x_residuals)
+        if hsic == 'r':
+            HSIC_x_to_y = dHSIC(x_test_column, y_residuals)
+            HSIC_y_to_x = dHSIC(y_test, x_residuals)
+            if HSIC_x_to_y.statistic < HSIC_y_to_x.statistic:
+                leakage_dir = 'No Leakage'
+            else:
+                leakage_dir = 'Leakage'
+            rank = HSIC_x_to_y.statistic - HSIC_y_to_x.statistic
+
+        dHSIC_xy = dHSIC(x_train_column, y_train)
+        p_val = dHSIC_xy.p_value
+        row = (leakage_dir, rank, p_val)
+        leakage_dict[col] = row
+    df = pd.DataFrame(leakage_dict).T
+    df.columns = ['Direction', 'Rank', 'P_value Independence X and Y']
+    return df.sort_values('Rank', ascending=False)
 
 
-        if HSIC_x_to_y.empirical_test()< HSIC_y_to_x.empirical_test():
-            leakage_prob[col] = 'No Leakage'
-        else:
-            leakage_prob[col] = 'Leakage'
-
-    for col in leakage_prob:
-        print "Column "+ str(col)+ ' was '+ leakage_prob[col]
-
-    keys = leakage_prob.keys()
-    keys.sort(reverse=True)
-    for key in keys:
-        print "The probability of column: " + str(leakage_prob[key]) + " is: " + str(key)
 
 def ANM_algorithm_pairwise(X, y, hsic='r'):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
@@ -71,8 +84,6 @@ def ANM_algorithm_pairwise(X, y, hsic='r'):
     if hsic=='r':
         HSIC_x_to_y = dHSIC(x_test_column, y_residuals)
         HSIC_y_to_x = dHSIC(y_test, x_residuals)
-
-
         if HSIC_x_to_y.statistic< HSIC_y_to_x.statistic:
             direction = 1
         else:
