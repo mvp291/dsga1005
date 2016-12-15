6e6a18fef5027e0ca6d840711251ea339cd5f73b
sb5518 <sb5518@nyu.edu>
2016-12-15 01:02:37 -0500

    Modified version of pairwise ANM with new R HSIC

--- code/additive_noise.py
+++ code/additive_noise.py
@@ -5,7 +5,7 @@ import hsic as hsic2
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import rbf_kernel, laplacian_kernel
 from independence_test import *
-
+from r_independence import *
 
 
 def compute_gp_regression(X_train, y_train, X_test):
@@ -14,7 +14,6 @@ def compute_gp_regression(X_train, y_train, X_test):
     k = pyGPs.cov.RBF()
     model.setPrior(mean=m, kernel=k)
     model.optimize(X_train, y_train)
-    print('Optimized negative log marginal likelihood:', round(model.nlZ,3))
     y_pred, _, _, _, _ = model.predict(X_test)
     return y_pred
 
@@ -49,7 +48,7 @@ def ANM_algorithm(X, y):
     for key in keys:
         print "The probability of column: " + str(leakage_prob[key]) + " is: " + str(key)
 
-def ANM_algorithm_pairwise(X, y):
+def ANM_algorithm_pairwise(X, y, hsic='r'):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     x_train_column = X_train
     x_test_column = X_test
@@ -60,12 +59,22 @@ def ANM_algorithm_pairwise(X, y):
     y_residuals = y_test - y_pred.ravel()  # esto no deberia ser absolute value?
     x_residuals = x_test_column - x_pred.ravel()
 
-    HSIC_x_to_y = HSIC_b(x_test_column, y_residuals)
-    HSIC_y_to_x = HSIC_b(y_test, x_residuals)
+    if hsic=='py':
+        HSIC_x_to_y = HSIC_b(x_test_column, y_residuals)
+        HSIC_y_to_x = HSIC_b(y_test, x_residuals)
+        if HSIC_x_to_y.empirical_test()< HSIC_y_to_x.empirical_test():
+            direction = 1
+        else:
+            direction = -1
+        return direction
+
+    if hsic=='r':
+        HSIC_x_to_y = dHSIC(x_test_column, y_residuals)
+        HSIC_y_to_x = dHSIC(y_test, x_residuals)
 
 
-    if HSIC_x_to_y.empirical_test()< HSIC_y_to_x.empirical_test():
-        direction = 1
-    else:
-        direction = -1
-    return direction
+        if HSIC_x_to_y.statistic< HSIC_y_to_x.statistic:
+            direction = 1
+        else:
+            direction = -1
+        return direction
